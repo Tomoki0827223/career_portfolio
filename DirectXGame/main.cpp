7b975b06a116b133717e9579996868a368e40627
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2026-01-19 14:50:37 +0900

    dawdasdaw

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -11,7 +11,7 @@ using namespace KamataEngine;
 // Scene enumに Tutorial と GameOver を追加
 enum class Scene { Title, Tutorial, Game, GameOver };
 
-Scene scene = Scene::Title;
+Scene scene = Scene::Game;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
